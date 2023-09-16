4376a6ee6872675507a24598628c6e1a67ccf66f
TKwong0338 <terencewong0338@gmail.com>
2023-09-16 15:58:11 +0800

    monster

--- Assets/Script/Monster.cs
+++ Assets/Script/Monster.cs
@@ -16,6 +16,9 @@ public class Monster : MonoBehaviour
     float tempSpeed2;
     public Animator anim;
 
+    public Transform player;
+    public bool isFlipped = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -95,6 +98,23 @@ public class Monster : MonoBehaviour
         mon2.speed = tempSpeed2;
     }
 
+    public void LookAtPlayer()
+    {
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
 
+        if (transform.position.x > player.position.x && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+        }
+        else if (transform.position.x < player.position.x && !isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
+        }
+    }
 
 }
