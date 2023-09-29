60eaf57fe82c2fff71587a873586eb93d8a9aded
Caslu426 <lucaschong426@gmail.com>
2023-09-29 16:01:57 +0800

    added checkfloor

---
+++ Assets/Script/CheckFloor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckFloor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("ground"))
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 100;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+    }
+
+
+
+
+}
--- Assets/Script/Monster.cs
+++ Assets/Script/Monster.cs
@@ -103,12 +103,15 @@ public class Monster : MonoBehaviour
             transform.localScale = flipped;
             transform.Rotate(0f, 180f, 0f);
             isFlipped = false;
+
         }
         else if (transform.position.x < player.position.x && !isFlipped)
         {
+           
             transform.localScale = flipped;
             transform.Rotate(0f, 180f, 0f);
             isFlipped = true;
+
         }
     }
 
